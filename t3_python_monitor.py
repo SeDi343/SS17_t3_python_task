877c9136276ea34680b15ed2fdf862235628a903
SeDi343 <sedi343@gmail.com>
2017-06-09 17:24:04 +0200

    Formated user output

--- t3_python_monitor.py
+++ t3_python_monitor.py
@@ -7,6 +7,7 @@
 # \author Sebastian Dichler <el16b032@technikum-wien.at> <sedi343@gmail.com>
 #
 # \version Rev.: 01, 09.06.2017 - Created the py file
+#          Rev.: 02, 09.06.2017 - Formated User output
 #
 
 import sys
@@ -31,27 +32,41 @@ core_logic = psutil.cpu_count()
 core_nologic = psutil.cpu_count(logical=False)
 
 while True:
+	
+# COLLECT SYSTEM INFORMATION
+	
 	cpu_percent_full = psutil.cpu_percent()
 	cpu_percent = psutil.cpu_percent(percpu=True)
 	cpu_frequency = psutil.cpu_freq()
 	cpu_temp = psutil.sensors_temperatures()
 	cpu_fan = psutil.sensors_fans()
+	disk = psutil.disk_usage('/')
+	battery = psutil.sensors_battery()
+	users = psutil.users()
+	
+# CLEAR SCREEN
 	
 	clear()
 	
-	print("Welcome to el16b032 python status monitor\n")
+# TERMINAL OUTPUT
 	
-	print("Your system has", core_logic, "logical cores and",
-			core_nologic, "physical cores")
+	print("System Status Monitor el16b032 BEL2 2017\n")
+	
+	print(core_logic, "logical cores", core_nologic, "physical cores")
 	print("")
 	
-	print("CPU Percentage\t\t", cpu_percent_full)
-	print("CPU Percentage\t\t", cpu_percent)
+# PRINT SYSTEM INFORMATION
+	
+	print("CPU Percentage\t\t", cpu_percent_full,"%", cpu_percent)
 	print("CPU Frequency\t\t",
-			"current:", cpu_frequency[0],
-			" min:", cpu_frequency[1],
-			" max:", cpu_frequency[2])
-			
+			"current:", cpu_frequency[0], "\n\t\t\t",
+			"min:", cpu_frequency[1], "\n\t\t\t",
+			"max:", cpu_frequency[2])
+	print("")
+	
+	print("Disk usage\t\t", disk[3])
+	print("")
+	
 	print("CPU Temperature\t\t")
 	names = set(list(cpu_temp.keys()))
 	for name in names:
@@ -68,5 +83,13 @@ while True:
 		print("CPU Fanspeed Left\t", cpu_fan[key][1][1], "rpm")
 	print("")
 	
+	print("Battery\t",
+		"capacity\t", battery[0], "\n\t",
+		"time left\t", secs2hours(battery[1]))
+	print("")
+	
+	print("Username\t\t", users[0][0])
+	print("Host\t\t\t", users[0][2])
+
 	
 	time.sleep(1)
