05d49a511cbb9cbbfb8ed3297fdcf9c53346a612
SeDi343 <sedi343@gmail.com>
2017-06-27 16:07:05 +0200

    Added try/exceptions and code fixing

--- t3_python_monitor.py
+++ t3_python_monitor.py
@@ -13,12 +13,18 @@
 #                                 cpu_frequency[3]
 #          Rev.: 04, 09.06.2017 - Added online user ammount
 #          Rev.: 05, 09.06.2017 - Added singal handler for ctrl-c
+#          Rev.: 06, 27.06.2017 - Adding try/exceptions
 #
 # \information Information from
 #              https://pythonhosted.org/psutil/
 #              https://stackoverflow.com/questions/14319023/find-out-who-is-logged-in-on-linux-using-python
 #              https://stackoverflow.com/questions/1112343/how-do-i-capture-sigint-in-python
 #
+
+
+################################################################################
+# I M P O R T   L I B R A R Y S                                                #
+################################################################################
 
 import sys
 import psutil
@@ -27,10 +33,9 @@ import time
 import datetime
 import signal
 
-
-# \033[1m
-
-debug = 0
+################################################################################
+# F U N C T I O N S                                                            #
+################################################################################
 
 def clear():
 	os.system("cls" if os.name == "nt" else "clear")
@@ -44,42 +49,109 @@ def signal_handler(signal, frame):
 	print("You pressed Ctrl+C!")
 	sys.exit(0)
 
-core_logic = psutil.cpu_count()
-core_nologic = psutil.cpu_count(logical=False)
+################################################################################
+# R E C E I V E   O N E   T I M E   N E E D E D   V A L U E S                  #
+################################################################################
+# CPU ammount logical
+try:
+	core_logic = psutil.cpu_count()
+except:
+	core_logic = "\033[1mNO DATA RECEIVED\033[0m"
+
+# CPU ammount physical
+try:
+	core_nologic = psutil.cpu_count(logical=False)
+except:
+	core_nologic = "\033[1mNO DATA RECEIVED\033[0m"
+
+################################################################################
+# R E C E I V E   V A L U E S                                                  #
+################################################################################
 
 signal.signal(signal.SIGINT, signal_handler)
 while True:
+# CPU overall percentage
+	try:
+		cpu_percent_full = psutil.cpu_percent(interval=None)
+	except:
+		cpu_percent_full = "\033[1mNO DATA RECEIVED\033[0m"
 	
-# COLLECT SYSTEM INFORMATION
+# CPU percentage per cpu
+	try:
+		cpu_percent = psutil.cpu_percent(interval=None, percpu=True)
+	except:
+		cpu_percent = "\033[1mNO DATA RECEIVED\033[0m"
 	
-	cpu_percent_full = psutil.cpu_percent(interval=None)
-	cpu_percent = psutil.cpu_percent(interval=None, percpu=True)
-	cpu_frequency = psutil.cpu_freq()
-	cpu_temp = psutil.sensors_temperatures()
-	memory = psutil.virtual_memory()
-	cpu_fan = psutil.sensors_fans()
-	disk = psutil.disk_usage('/')
-	battery = psutil.sensors_battery()
-	users = psutil.users()
-	userammount = os.popen('who | wc -l').read()
-	userammount = userammount.rstrip()
+# CPU frequency
+	try:
+		cpu_frequency = psutil.cpu_freq()
+	except:
+		cpu_frequency = "\033[1mNO DATA RECEIVED\033[0m"
 	
-# CLEAR SCREEN
+# CPU temperarature
+	try:
+		cpu_temp = psutil.sensors_temperatures()
+	except:
+		cpu_temp = "\033[1mNO DATA RECEIVED\033[0m"
+	
+# MEMORY information
+	try:
+		memory = psutil.virtual_memory()
+	except:
+		memory = "\033[1mNO DATA RECEIVED\033[0m"
+	
+# CPU fan
+	try:
+		cpu_fan = psutil.sensors_fans()
+	except:
+		cpu_fan = "\033[1mNO DATA RECEIVED\033[0m"
+	
+# DISK information
+	try:
+		disk = psutil.disk_usage('/')
+	except:
+		disk = "\033[1mNO DATA RECEIVED\033[0m"
+	
+# BATTERY information
+	try:
+		battery = psutil.sensors_battery()
+	except:
+		battery = "\033[1mNO DATA RECEIVED\033[0m"
+	
+# USERS logged in user
+	try:
+		users = psutil.users()
+	except:
+		users = "\033[1mNO DATA RECEIVED\033[0m"
+	
+# USERS ammount of logged in users
+	try:
+		userammount = os.popen('who | wc -l').read()
+		userammount = userammount.rstrip()
+	except:
+		userammount = "\033[1mNO DATA RECEIVED\033[0m"
+	
+################################################################################
+# C L E A R   S C R E E N                                                      #
+################################################################################
 	
 	clear()
 	
-# TERMINAL OUTPUT
+################################################################################
+# T E R M I N A L   O U T P U T                                                #
+################################################################################
 	
+# HEADERBAR
 	print("\033[1mSystem Status Monitor / el16b032 / BEL2 2017\033[0m\n")
 	print("Hello\033[1m", users[0][0], "\033[0mThere", userammount, "users online")
 	
-	print("\033[1m", core_logic, "\033[0mlogical cores and\033[1m", core_nologic, "\033[0mphysical cores")
+	print("You have\033[1m", core_logic, "\033[0mlogical cores and\033[1m", core_nologic, "\033[0mphysical cores")
 	if memory.available <= 100 * 1024 * 1024:
-		print("Warning: Less Memory available:", memory.available)
+		print("\033[1mWarning: Less Memory available:", memory.available, "\033[0m")
 	
 	print("")
 	
-# PRINT SYSTEM INFORMATION
+# PRINT CPU INFORMATION
 	
 	print("CPU Percentage\t\t\033[1m", cpu_percent_full,"%\033[0m", cpu_percent)
 	print("CPU Frequency\t\t",
@@ -88,11 +160,7 @@ while True:
 			"max:", cpu_frequency.max)
 	print("")
 	
-	
-	print("Disk usage\t\t\033[1m", disk.percent, "\033[0m")
-	print("")
-	
-	
+# PRINT CPU TEMPERATURE INFORMATION
 	
 	names = set(list(cpu_temp.keys()))
 	for name in names:
@@ -104,22 +172,29 @@ while True:
 						entry.critical)
 	print("")
 	
+# PRINT DISK INFORMATION
+	
+	print("Disk usage\t\t\033[1m", disk.percent, "\033[0m")
+	print("")
+	
+# PRINT FAN SPEED INFORMATION
 	
 	for name, entries in cpu_fan.items():
 		for entry in entries:
 			print("Fanspeed", entry.label or name, "\t\033[1m", entry.current, "rpm\t\033[0m")
 	print("")
 	
+# PRINT BATTERY INFORMATION
 	
 	print("Battery\t",
 		"capacity\t\033[1m", battery.percent, "\033[0m\n\t",
 		"time left\t", secs2hours(battery.secsleft))
 	print("")
 	
+# PRINT USER AND HOSTNAME
 	
-	print("Username\t\t\033[1m", users[0][0], "\033[0m@", users[0][2])
-	
-	
+	for i in range(0, len(users)):
+		print("Username\t\t\033[1m", users[i].name, "\033[0m@", users[i].host)
 	
 	time.sleep(1)
 signal.pause()
